91bb2a13d30160099f9b859ed01ff7d66828505d
uniOpifex <njhunit@gmail.com>
2017-10-03 11:07:59 -0400

    refractored postController create route

--- routes/postController.js
+++ routes/postController.js
@@ -4,6 +4,7 @@ const router = express.Router({ mergeParams: true })
 
 const Schema = require("../db/schema.js");
 const BoardModel = Schema.BoardModel;
+const UserModel = Schema.UserModel;
 
 // INDEX route
 router.get('/', (request, response) => {
@@ -34,7 +35,6 @@ router.get('/new', (request, response) => {
 
     // GRAB the board ID from the parameters
     const boardId = request.params.boardId
-    console.log(request.params)
 
     // RENDER a new form for a fresh Snowboard,
     // also passing the boardId to use in the
@@ -51,27 +51,36 @@ router.post('/', (request, response) => {
     const boardId = request.params.boardId
     // const userId = request.params.userId
     // GRAB the new snowboard info from the request body
-    const newPost = request.body
-    
-        // Use the CompanyModel to find the company by ID in the database
-    
-    // USE the BoardModel to find the board by ID
-    BoardModel.findById(boardId)
-        .then((board) => {
-            UserModel.findById(userId)
-                .then((user) => {
+    const body = request.body
+    // console.log('test 1' + request.body)
+    var newPost = {}
+    newPost.content = body.content
+    // console.log('test 2' + newPost)
+    UserModel.find({hash: body.hash})
+        .then((user) => {
                     // THEN once the user comes back from the database,
                     // render the single company's info using Handlebars
-                    response.render('users/show', {
-                        user: user
-                    })
-                 })
-                .catch((error) => {
-                    console.log(error)
-                    })
+            // console.log("newPost")
+            // console.log(newPost)
+            // console.log(user)
+            console.log(user)
+            console.log(user[0].name)
+            newPost.user = user[0].name
+            console.log(newPost)
+            // console.log(newPost)
+            // console.log("user");
+            // console.log(user)
+        })
+        .then((user) => {
+            return BoardModel.findById(boardId)
+        })
+        .then((board) => {
+            
             // THEN once you have found the board from the database
             // PUSH the new snowboard object into the board's 
-            // snowboard array            
+            // snowboard array 
+            // console.log("newPost")  
+            // console.log(newPost)         
             board.posts.push(newPost)
 
             // SAVE the board and return the PROMISE
@@ -82,6 +91,20 @@ router.post('/', (request, response) => {
             // REDIRECT to the Snowboards index for that board
             response.redirect(`/boards/${boardId}/posts`)
         })
+        .catch((error) => {
+            console.log(error)
+        })
+        // Use the CompanyModel to find the company by ID in the database
+    
+    // USE the BoardModel to find the board by ID
+        
+
+        
+        // .catch((error) => {
+        //     console.log(error)
+        // })
+    
+
 
 })
 
@@ -114,39 +137,39 @@ router.post('/', (request, response) => {
 // })
 
 // UPDATE route
-router.put('/postId', (request, response) => {
+// router.put('/postId', (request, response) => {
 
-    // GRAB the board ID from the parameters
-    const boardId = request.params.boardId
+//     // GRAB the board ID from the parameters
+//     const boardId = request.params.boardId
 
-    // GRAB the snowboard ID from the parameters
-    const postId = request.params.postId
+//     // GRAB the snowboard ID from the parameters
+//     const postId = request.params.postId
 
-    // GRAB the updated snowboard object from the request body
-    const updatedPost = request.body
+//     // GRAB the updated snowboard object from the request body
+//     const updatedPost = request.body
 
-    // USE the BoardModel to find the board by ID
-    BoardModel.findById(boardId)
-        .then((board) => {
-            // THEN once the board has been returned,
-            // FIND the snowboard by ID from the board's snowboards
-            const boardId = board.post.id(postId)
+//     // USE the BoardModel to find the board by ID
+//     BoardModel.findById(boardId)
+//         .then((board) => {
+//             // THEN once the board has been returned,
+//             // FIND the snowboard by ID from the board's snowboards
+//             const boardId = board.post.id(postId)
 
-            // MAP each attribute from the updated snowboard object to
-            // the same attribute on the original snowboard
-            post.content = updatedPost.content
-            snowboard.price = updatedSnowboard.price
+//             // MAP each attribute from the updated snowboard object to
+//             // the same attribute on the original snowboard
+//             post.content = updatedPost.content
+//             snowboard.price = updatedSnowboard.price
 
-            // SAVE the updated board and return the PROMISE
-            return board.save()
-        })
-        .then(() => {
-            // THEN once the board has saved, REDIRECT to the 
-            // snowboard's SHOW page
-            response.redirect(`/boards/${boardId}/posts/`)
-        })
+//             // SAVE the updated board and return the PROMISE
+//             return board.save()
+//         })
+//         .then(() => {
+//             // THEN once the board has saved, REDIRECT to the 
+//             // snowboard's SHOW page
+//             response.redirect(`/boards/${boardId}/posts/`)
+//         })
 
-})
+// })
 
 // // SHOW route
 // router.get('/:snowboardId', (request, response) => {
@@ -201,6 +224,5 @@ router.put('/postId', (request, response) => {
 //             response.redirect(`/companies/${boardId}/snowboards`)
 //         })
 // })
-
 
 module.exports = router;
